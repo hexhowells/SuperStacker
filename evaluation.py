1e7216fd060097dadb09b62bca47169fb2f110df
Morgan <58524460+hexhowells@users.noreply.github.com>
2022-07-02 22:29:39 +0100

    find locations where pieces can be placed

--- evaluation.py
+++ evaluation.py
@@ -1,27 +1,22 @@
 import numpy as np
-
-
-def get_value(board, piece, x, y):
-	pass
-	# evaluate based on following
-	#   line clears
-	#   addition block height
-	#   holes
-	#   wells
+import pieces
+from process_data import renderPiece
 
 
 # any empty cell with a solid cell above it
 def holes(board):
 	count = 0
+	coords = []
 	for row in range(1,20):
 		for col in range(10):
 			if (board[row][col] == 0) and (board[row-1][col] != 0):
 				count += 1
-	return count
+				coords.append((row, col))
+	return count, coords
 
 
 # count number of rows with no empty cells
-def line_clear(board):
+def line_clears(board):
 	count = 0
 	for row in range(20):
 		full = sum([1 if x != 0 else 0 for x in board[row]])
@@ -62,13 +57,79 @@ def added_height(cur_height, new_height):
 	return max(0, (new_height - cur_height))
 
 
-
 def surface_heights(board):
-	heights = [-1] * 10
+	heights = [20] * 10
 	for col in range(10):
 		for row in range(20):
 			if board[row][col] != 0:
 				heights[col] = row
 				break
 	return heights
+
+
+def render_board(board):
+	for row in range(20):
+		print(''.join(['#' if x != 0 else '.' for x in board[row]]))
+
+
+# (x, y) = bottom left corner of the pieces hitbox
+def render_piece(x, y, pieceID):
+	piece_array = pieces.renders[pieceID]
+	
+	height = len(piece_array)
+	width = len(piece_array[0])
+	x -= (height - 1)
+
+	coords = []
+
+	for row in range(height):
+		for col in range(width):
+			if piece_array[row][col] == 1:
+				coords.append((x+row, y+col))
+
+	return coords
+
+
+def is_valid_placement(board, tile_coords, holes):
+	for (x, y) in tile_coords:
+		# tile in solid block or hole
+		if (board[x][y] != 0) or ((x, y) in holes):
+			return False
+
+	for (x, y) in tile_coords:
+		_x = x + 1
+		if _x == 20:
+			return True
+		if board[_x][y] != 0:
+			return True
+
+	return False
+
+
+
+def get_placeable_area(board, pieceID='0x4'):
+	heights = surface_heights(board)
+	low_bound = max(heights)
+	high_bound = max( 0, (min(heights) - 1) )
+
+	max_col = 10 - pieces.col_offsets[pieceID]
+
+	valids = []
+
+	_, _holes = holes(board)
+
+	for row in range(high_bound, low_bound):
+		for col in range(0, max_col):
+			tile_coords = render_piece(row, col, pieceID)
+
+			if is_valid_placement(board, tile_coords, _holes):
+				valids.append([pieceID, row, col])
+
+				_board = np.zeros((20, 10))
+				for (x, y) in tile_coords:
+					_board[x][y] = 1
+				render_board(_board)
+	
+	return valids
+				
 
